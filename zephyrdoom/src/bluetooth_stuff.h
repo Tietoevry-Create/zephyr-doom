c648ccbc0b3009a2091d37933103427c93e9457c
Marek Ličko <marek.licko@tietoevry.com>
2024-09-23 20:00:45 +0200

    Some changes to make the joystick controls more accurate

--- zephyrdoom/src/bluetooth_stuff.h
+++ zephyrdoom/src/bluetooth_stuff.h
@@ -104,21 +104,20 @@ uint8_t ble_data_received(struct bt_nus_client *nus, const uint8_t *data,
            adc_buf0, adc_buf1, button_A, button_B, button_C, button_D, button_E,
            button_F);
     
-    int centered_adc = adc_buf0 - 475;
-    int deadzone = 30;
-    if (centered_adc > -deadzone && centered_adc < deadzone) {
+    int centered_adc = adc_buf0 - 470;
+    if (centered_adc > -50 && centered_adc < 50) {
         adc_buf0 = 0;
     } else {
         adc_buf0 = centered_adc;
     }
-    centered_adc = adc_buf1 - 475;
-    if (centered_adc > -deadzone && centered_adc < deadzone) {
+    centered_adc = adc_buf1 - 480;
+    if (centered_adc > -40 && centered_adc < 40) {
         adc_buf1 = 0;
     } else {
         adc_buf1 = centered_adc;
     }
-    adc_buf0 /= 10;
-    adc_buf1 /= 10;
+    adc_buf0 /= 15;
+    adc_buf1 /= 15;
     adc_buf0 = -adc_buf0;
     adc_buf1 = -adc_buf1;
 
