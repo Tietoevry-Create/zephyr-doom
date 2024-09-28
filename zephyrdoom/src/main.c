ad15f33dab57a6fb9bc95f4fd51c838a761ff9fc
Marek Ličko <marek.licko@tietoevry.com>
2024-09-28 17:21:50 +0200

    Cleanup

--- zephyrdoom/src/main.c
+++ zephyrdoom/src/main.c
@@ -60,7 +60,6 @@ void setup_lcd_pins_old() {
 void clock_initialization() {
     nrfx_clock_hfclk_start();
     nrf_clock_hfclk_div_set(NRF_CLOCK_S, NRF_CLOCK_HFCLK_DIV_1);
-    // NRF_CLOCK_S->HFCLKCTRL = (CLOCK_HFCLKCTRL_HCLK_Div1 << CLOCK_HFCLKCTRL_HCLK_Pos);
     nrfx_clock_divider_set(NRF_CLOCK_DOMAIN_HFCLK192M, NRF_CLOCK_HFCLK_DIV_1);
     // nrfx_clock_divider_set(NRF_CLOCK_DOMAIN_HFCLK, NRF_CLOCK_HFCLK_DIV_1);
 }
@@ -273,13 +272,9 @@ int main(void) {
 
     N_ButtonsInit();
 
-    // // N_I2S_init();
-
     M_ArgvInit();
 
-    // bluetooth_main();
     bluetooth_init();
-    // bluetooth_main();
 
     D_DoomMain();
 
