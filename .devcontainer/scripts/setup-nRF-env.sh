f92eff58a546cc1ab2ce0b02a5ea0e2673ede5b1
zvakkmar <martin.zvak@tietoevry.com>
2025-08-07 14:31:29 +0200

    [UPDATE] - changes required to expose USB devices to container - added Install Segger JLink to enable flashing of embedded target - added Install nRF command line tools to enable flashing of embedded target

--- .devcontainer/scripts/setup-nRF-env.sh
+++ .devcontainer/scripts/setup-nRF-env.sh
@@ -7,8 +7,17 @@ SDK_VERSION=v2.6.0
 
 SDK_DIR_PATH="$HOME/ncs/$SDK_VERSION"
 
+# Install Segger JLink
+sudo dpkg --unpack /home/developer/JLink_Linux_V794e_x86_64.deb
+sudo rm /var/lib/dpkg/info/jlink.postinst -f
+sudo dpkg --force-all --configure jlink
+
+# Install nRF command line tools
+sudo dpkg -i /home/developer/nrf-command-line-tools_10.24.2_amd64.deb 
+
+# Install nRF toolchain and SDK
 if [[ ! -d "$SDK_DIR_PATH" ||
-      -z $(ls -A "$SDK_DIR_PATH") ]]; then
+      -z $(ls -A "$SDK_DIR_PATH") ]]; then          
     nrfutil self-upgrade
     nrfutil install device
     nrfutil install toolchain-manager
