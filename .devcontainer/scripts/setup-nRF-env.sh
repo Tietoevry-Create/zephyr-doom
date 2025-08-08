1c4ace6d1d25ac41c3f8944553d26e91cfd83d0b
Maciej Gebicz <maciej.gebicz@tietoevry.com>
2025-08-08 12:18:44 +0200

    devcontainer: Improve installation of flashing tools
    The commit contains the following changes:
    - Add *.deb packages to the .gitignore files;
    - Move installation of nRF command line (required for backward
      compatibility) to the Dockerfile;
      - Remove unnecessary mountpoint of mentioned package;
    - Change mounting of required "/dev" directory from Docker's "--volume"
      option for more expressive, portable and standard compliant "mounts"
      feature of DevContainer;
    - Clean whitespaces;
    - Improve installation of Segger J-Link tool;
      - Install only if not present;
      - Install dependency packages;
      - Move it after installation of nRF toolchan and SDK to avoid
        detection issues during main installation process;
    - Remove redundant mountpoint of Link_Linux_V794e_x86_64.deb package
      which is automatically picked up if it is present in a directory of
      the repository;
    - Add information in the README about manual SEGGER J-Link package
    installation;
    - Bump versions of various extensions;
    - Fix incorrectly resolved ZEPHYR_BASE env variable (in this context
      $HOME is not resolved, but treated literally);

--- .devcontainer/scripts/setup-nRF-env.sh
+++ .devcontainer/scripts/setup-nRF-env.sh
@@ -4,26 +4,29 @@ set -euo pipefail
 
 TOOLCHAIN_VERSION=v2.6.2
 SDK_VERSION=v2.6.0
+JLINK_PKG_NAME="JLink_Linux_V794e_x86_64.deb"
 
 SDK_DIR_PATH="$HOME/ncs/$SDK_VERSION"
-
-# Install Segger JLink
-sudo dpkg --unpack /home/developer/JLink_Linux_V794e_x86_64.deb
-sudo rm /var/lib/dpkg/info/jlink.postinst -f
-sudo dpkg --force-all --configure jlink
-
-# Install nRF command line tools
-sudo dpkg -i /home/developer/nrf-command-line-tools_10.24.2_amd64.deb 
+JLINK_PKG_PATH="/workspaces/zephyr-doom/.devcontainer/${JLINK_PKG_NAME}"
+DPKG_INFO_PATH="/var/lib/dpkg/info"
 
 # Install nRF toolchain and SDK
 if [[ ! -d "$SDK_DIR_PATH" ||
-      -z $(ls -A "$SDK_DIR_PATH") ]]; then          
+      -z $(ls -A "$SDK_DIR_PATH") ]]; then
     nrfutil self-upgrade
     nrfutil install device
     nrfutil install toolchain-manager
     nrfutil toolchain-manager install --ncs-version "$TOOLCHAIN_VERSION"
     nrfutil install sdk-manager
     nrfutil sdk-manager install "$SDK_VERSION"
+fi
+
+# Install SEGGER J-Link
+if ! dpkg -s jlink &>/dev/null; then
+  sudo dpkg --unpack "$JLINK_PKG_PATH"
+  sudo rm -f "${DPKG_INFO_PATH}/jlink.postinst"
+  sudo dpkg --force-all --configure jlink
+  sudo apt-get -y install -f
 fi
 
 echo "Finished execution of $(basename "${0}") ..."
