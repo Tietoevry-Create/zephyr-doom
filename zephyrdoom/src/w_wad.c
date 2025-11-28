0203181e8eb4dfc3a174e34318a610005aa0fef1
Marek <mareklicko448@gmail.com>
2025-11-28 09:58:25 +0100

    Cleaned up w_wad

--- zephyrdoom/src/w_wad.c
+++ zephyrdoom/src/w_wad.c
@@ -18,7 +18,6 @@
 
 #include <ctype.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #ifndef SEGGER
 #include <strings.h>
@@ -28,15 +27,11 @@
 #include "i_swap.h"
 #include "i_system.h"
 #include "i_timer.h"
-#include "i_video.h"
-#include "m_misc.h"
 #include "n_buttons.h"
-#include "v_diskicon.h"
 #include "w_wad.h"
 #include "z_zone.h"
 
 typedef PACKED_STRUCT({
-    // Should be "IWAD" or "PWAD".
     char identification[4];
     int numlumps;
     int infotableofs;
@@ -48,22 +43,18 @@ typedef PACKED_STRUCT({
     char name[8];
 }) filelump_t;
 
-#include <stdlib.h>  // For Z_Malloc (assuming this is like malloc) - replaced with k_malloc
-#include <string.h>  // For strncmp, strlen, strcasecmp, memcmp
 #include <zephyr/device.h>
 #include <zephyr/drivers/flash.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/fs/fs.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-#include <zephyr/sys/util.h>  // For MIN
+#include <zephyr/storage/disk_access.h>
+#include <zephyr/sys/util.h>
 
 #include "n_fs.h"
 #include "n_mem.h"
 #include "n_qspi.h"
-
-// --- Required Includes from both examples ---
-#include <zephyr/storage/disk_access.h>  // If disk init/mount is done elsewhere
 
 LOG_MODULE_REGISTER(w_wad, LOG_LEVEL_INF);
 
@@ -75,38 +66,26 @@ LOG_MODULE_REGISTER(w_wad, LOG_LEVEL_INF);
     "Unsupported board: spi_flash0 devicetree alias is not defined or disabled."
 #endif
 
-// --- Filesystem Configuration (Assuming FATFS as in the first example) ---
-// Adapt if using EXT2 or another filesystem
 #define DISK_DRIVE_NAME "SD"
 #define DISK_MOUNT_PT "/" DISK_DRIVE_NAME ":"
 
-extern int no_sdcard;  // NRFD-NOTE: from main.c
-//
-// GLOBALS
-//
-
-// wad_file_t *wad_file = NULL; // NRFD
-
-// Location of each lump on disk.
+extern int no_sdcard;
 #define MAX_NUMLUMPS 1300
-// lumpinfo_t lumpinfo[MAX_NUMLUMPS];
 unsigned short numlumps = 0;
 
-filelump_t *filelumps;
+filelump_t* filelumps;
 
-// Hash table for fast lookups
-static lumpindex_t *lumphash = NULL;
+static lumpindex_t* lumphash = NULL;
 
 N_FILE wad_file;
 int first_lump_pos;
 
-// --- LED 3 fast flashing support during WAD copy ---
 static const struct gpio_dt_spec wad_led3 =
     GPIO_DT_SPEC_GET(DT_ALIAS(led3), gpios);
 static struct k_timer wad_led_timer;
 static bool wad_led_init_done;
 
-static void wad_led_timer_expiry(struct k_timer *timer) {
+static void wad_led_timer_expiry(struct k_timer* timer) {
     ARG_UNUSED(timer);
     gpio_pin_toggle_dt(&wad_led3);
 }
@@ -133,24 +112,7 @@ static void wad_led_flash_stop(void) {
     gpio_pin_set_dt(&wad_led3, 0);
 }
 
-/*
-int debugLumpCount = 0;
-int debugLumpNums[12];
-void *debugLumpCache[12];
-*/
-
-// Variables for the reload hack: filename of the PWAD to reload, and the
-// lumps from WADs before the reload file, so we can resent numlumps and
-// load the file again.
-/* NRFD-EXCLUDE
-static wad_file_t *reloadhandle = NULL;
-static lumpinfo_t *reloadlumps = NULL;
-static char *reloadname = NULL;
-static int reloadlump = -1;
-*/
-
-// Hash function used for lump names.
-unsigned int W_LumpNameHash(const char *s) {
+unsigned int W_LumpNameHash(const char* s) {
     // This is the djb2 string hash function, modded to work on strings
     // that have a maximum length of 8.
 
@@ -164,12 +126,12 @@ unsigned int W_LumpNameHash(const char *s) {
     return result;
 }
 
-wad_file_t *W_AddFile(char *filename) {
+wad_file_t* W_AddFile(char* filename) {
     lumpindex_t i;
-    wad_file_t *wad_file_data;
+    wad_file_t* wad_file_data;
     boolean do_wad_transfer = false;
 
-    const struct device *flash_dev = DEVICE_DT_GET(DT_ALIAS(spi_flash0));
+    const struct device* flash_dev = DEVICE_DT_GET(DT_ALIAS(spi_flash0));
     if (!device_is_ready(flash_dev)) {
         LOG_ERR("Flash device %s is not ready", flash_dev->name);
         return NULL;
@@ -224,7 +186,7 @@ wad_file_t *W_AddFile(char *filename) {
         N_qspi_reserve_blocks(num_blocks);
 
         if (!no_sdcard) {
-            uint8_t *block_data = N_malloc(N_QSPI_BLOCK_SIZE);
+            uint8_t* block_data = N_malloc(N_QSPI_BLOCK_SIZE);
             int block_loc = 0;
             boolean data_mismatch = do_wad_transfer;
             boolean led_flash_started = false;
@@ -354,9 +316,9 @@ wad_file_t *W_AddFile(char *filename) {
         // Read header from QSPI flash
         wadinfo_t header_buffer;
         flash_read(flash_dev, 0, &header_buffer, sizeof(wadinfo_t));
-        wadinfo_t *header_ptr = &header_buffer;
+        wadinfo_t* header_ptr = &header_buffer;
 
-        uint8_t *dat_buffer = k_malloc(300);
+        uint8_t* dat_buffer = k_malloc(300);
         if (dat_buffer != NULL) {
             flash_read(flash_dev, 0, dat_buffer, 300);
 
@@ -420,7 +382,7 @@ wad_file_t *W_AddFile(char *filename) {
         }
 
         first_lump_pos = header_ptr->infotableofs;
-        filelumps = (filelump_t *)N_qspi_data_pointer(first_lump_pos);
+        filelumps = (filelump_t*)N_qspi_data_pointer(first_lump_pos);
         numlumps += header_ptr->numlumps;
 
         wad_file_data->path = filename;
@@ -435,68 +397,24 @@ wad_file_t *W_AddFile(char *filename) {
     return wad_file_data;
 }
 
-void *W_LumpDataPointer(lumpindex_t lump) {
+void* W_LumpDataPointer(lumpindex_t lump) {
     return N_qspi_data_pointer(LONG(filelumps[lump].filepos));
 }
 
-//
-// W_NumLumps
-//
 int W_NumLumps(void) { return numlumps; }
 
-//
-// W_CheckNumForName
-// Returns -1 if name not found.
-//
-
-lumpindex_t W_CheckNumForName(const char *name) {
+lumpindex_t W_CheckNumForName(const char* name) {
     lumpindex_t i;
-
-    // Do we have a hash table yet?
-
-    /* NRFD-TODO: lump hash table */
-    /*
-    if (lumphash != NULL)
-    {
-        int hash;
-
-        // We do! Excellent.
-
-        hash = W_LumpNameHash(name) % numlumps;
-
-        for (i = lumphash[hash]; i != -1; i = lumpinfo[i]->next)
-        {
-            if (!strncasecmp(lumpinfo[i]->name, name, 8))
-            {
-                return i;
-            }
+    for (i = numlumps - 1; i >= 0; --i) {
+        if (!strncasecmp(filelumps[i].name, name, 8)) {
+            return i;
         }
     }
-    else*/
-    {
-        // We don't have a hash table generate yet. Linear search :-(
-        //
-        // scan backwards so patch lump files take precedence
-
-        for (i = numlumps - 1; i >= 0; --i) {
-            if (!strncasecmp(filelumps[i].name, name, 8))
-            // if (!strncasecmp(lumpinfo[i].name, name, 8))
-            {
-                return i;
-            }
-        }
-    }
-
-    // TFB. Not found.
 
     return -1;
 }
 
-//
-// W_GetNumForName
-// Calls W_CheckNumForName, but bombs out if not found.
-//
-lumpindex_t W_GetNumForName(const char *name) {
+lumpindex_t W_GetNumForName(const char* name) {
     lumpindex_t i;
 
     i = W_CheckNumForName(name);
@@ -508,55 +426,23 @@ lumpindex_t W_GetNumForName(const char *name) {
     return i;
 }
 
-char *W_LumpName(lumpindex_t lump) { return filelumps[lump].name; }
+char* W_LumpName(lumpindex_t lump) { return filelumps[lump].name; }
 
-//
-// W_LumpLength
-// Returns the buffer size needed to load the given lump.
-//
 int W_LumpLength(lumpindex_t lump) {
     if (lump >= numlumps) {
         I_Error("W_LumpLength: %i >= numlumps", lump);
     }
 
-    // return lumpinfo[lump].size;
     return LONG(filelumps[lump].size);
 }
 
-//
-// W_ReadLump
-// Loads the lump into the given buffer,
-//  which must be >= W_LumpLength().
-//
-void W_ReadLump(lumpindex_t lump, void *dest) {
+void W_ReadLump(lumpindex_t lump, void* dest) {
     if (lump >= numlumps) {
         I_Error("W_ReadLump: %i >= numlumps", lump);
     }
-
-    // lumpinfo_t *l;
-    // l = &lumpinfo[lump];
-    // printf("W_ReadLump(dummy): %.8s\n", l->name);
-
-    // V_BeginRead(l->size);
-    filelump_t *filelump = &filelumps[lump];
-    void *ptr = N_qspi_data_pointer(LONG(filelump->filepos));
+    filelump_t* filelump = &filelumps[lump];
+    void* ptr = N_qspi_data_pointer(LONG(filelump->filepos));
     memcpy(dest, ptr, LONG(filelump->size));
-
-    // memcpy(dest, l->cache, l->size);
-
-    /* NRFD-EXCLUDE
-
-    // int c;
-    // printf("Read lump at %d with size %d to %X\n", l->position, l->size,
-    (unsigned int)(dest));
-    // c = W_Read(l->wad_file, l->position, dest, l->size);
-    // c = W_Read(wad_file, l->position, dest, l->size);
-
-    if (c < l->size)
-    {
-        I_Error("W_ReadLump: only read %i of %i on lump %i",
-                c, l->size, lump);
-    }*/
 }
 
 //
@@ -571,69 +457,18 @@ void W_ReadLump(lumpindex_t lump, void *dest) {
 // when no longer needed (do not use Z_ChangeTag).
 //
 
-void *W_CacheLumpNum(lumpindex_t lumpnum, int tag) {
-    byte *result;
-    // lumpinfo_t *lump;
+void* W_CacheLumpNum(lumpindex_t lumpnum, int tag) {
+    byte* result;
 
     if ((unsigned)lumpnum >= numlumps) {
         I_Error("W_CacheLumpNum: %i >= numlumps", lumpnum);
     }
-
-    // lump = &lumpinfo[lumpnum];
-
-    // Get the pointer to return.  If the lump is in a Memory-mapped
-    // file, we can just return a pointer to within the memory-mapped
-    // region.  If the lump is in an ordinary file, we may already
-    // have it cached; otherwise, load it into memory.
-
-    // result = lump->cache;
-    /*
-    for (int i=0;i<debugLumpCount;i++) {
-        if (debugLumpNums[i]==lumpnum) {
-            byte *cache = debugLumpCache[i];
-            byte *qspi_data = W_LumpDataPointer(lumpnum);
-            for (int j=0;j<filelumps[lumpnum].size;j++) {
-                if (cache[j] != qspi_data[j]) {
-                    printf("X");
-                }
-            }
-            return cache;
-        }
-    }
-    */
     result = W_LumpDataPointer(lumpnum);
-    // N_ldbg("W_CacheLumpNum: %.8s\n", lump->name);
-
-    /* NRFD-EXCLUDE:
-    if (lump->wad_file->mapped != NULL)
-    {
-        // Memory mapped file, return from the mmapped region.
-
-        result = lump->wad_file->mapped + lump->position;
-    }
-    else if (lump->cache != NULL)
-    {
-        // Already cached, so just switch the zone tag.
-
-        result = lump->cache;
-        Z_ChangeTag(lump->cache, tag);
-    }
-    else
-    {
-        // Not yet loaded, so load it now
-        lump->cache = Z_Malloc(W_LumpLength(lumpnum), tag, &lump->cache);
-        W_ReadLump (lumpnum, lump->cache);
-        result = lump->cache;
-    }
-    */
 
     return result;
 }
 
-//
-// W_CacheLumpName
-//
-void *W_CacheLumpName(char *name, int tag) {
+void* W_CacheLumpName(char* name, int tag) {
     return W_CacheLumpNum(W_GetNumForName(name), tag);
 }
 
@@ -651,128 +486,12 @@ void W_ReleaseLumpNum(lumpindex_t lumpnum) {
     if ((unsigned)lumpnum >= numlumps) {
         I_Error("W_ReleaseLumpNum: %i >= numlumps", lumpnum);
     }
-
-    /* NRFD-EXCLUDE
-
-    lumpinfo_t *lump;
-    lump = &lumpinfo[lumpnum];
-
-    if (lump->wad_file->mapped != NULL)
-    {
-        // Memory-mapped file, so nothing needs to be done here.
-    }
-    else
-    {
-        Z_ChangeTag(lump->cache, PU_CACHE);
-    }
-    */
 }
 
-void W_ReleaseLumpName(char *name) { W_ReleaseLumpNum(W_GetNumForName(name)); }
-
-#if 0
-
-//
-// W_Profile
-//
-int             info[2500][10];
-int             profilecount;
-
-void W_Profile (void)
-{
-    int         i;
-    memblock_t* block;
-    void*       ptr;
-    char        ch;
-    FILE*       f;
-    int         j;
-    char        name[9];
-
-
-    for (i=0 ; i<numlumps ; i++)
-    {
-        ptr = lumpinfo[i].cache;
-        if (!ptr)
-        {
-            ch = ' ';
-            continue;
-        }
-        else
-        {
-            block = (memblock_t *) ( (byte *)ptr - sizeof(memblock_t));
-            if (block->tag < PU_PURGELEVEL)
-                ch = 'S';
-            else
-                ch = 'P';
-        }
-        info[i][profilecount] = ch;
-    }
-    profilecount++;
-
-    f = fopen ("waddump.txt","w");
-    name[8] = 0;
-
-    for (i=0 ; i<numlumps ; i++)
-    {
-        memcpy (name,lumpinfo[i].name,8);
-
-        for (j=0 ; j<8 ; j++)
-            if (!name[j])
-                break;
-
-        for ( ; j<8 ; j++)
-            name[j] = ' ';
-
-        fprintf (f,"%s ",name);
-
-        for (j=0 ; j<profilecount ; j++)
-            fprintf (f,"    %c",info[i][j]);
-
-        fprintf (f,"\n");
-    }
-    fclose (f);
-}
-
-#endif
-
-// Generate a hash table for fast lookups
+void W_ReleaseLumpName(char* name) { W_ReleaseLumpNum(W_GetNumForName(name)); }
 
 void W_GenerateHashTable(void) {
-    lumpindex_t i;
     printf("NRDF-TODO? W_GenerateHashTable\n");
-
-    /*
-    // Free the old hash table, if there is one:
-    if (lumphash != NULL)
-    {
-        Z_Free(lumphash);
-    }
-
-    // Generate hash table
-    if (numlumps > 0)
-    {
-        lumphash = Z_Malloc(sizeof(lumpindex_t) * numlumps, PU_STATIC, NULL);
-
-        for (i = 0; i < numlumps; ++i)
-        {
-            lumphash[i] = -1;
-        }
-
-        for (i = 0; i < numlumps; ++i)
-        {
-            unsigned int hash;
-
-            hash = W_LumpNameHash(lumpinfo[i]->name) % numlumps;
-
-            // Hook into the hash table
-
-            lumpinfo[i]->next = lumphash[hash];
-            lumphash[hash] = i;
-        }
-    }
-    */
-
-    // All done!
 }
 
 // The Doom reload hack. The idea here is that if you give a WAD file to -file
@@ -782,65 +501,5 @@ void W_GenerateHashTable(void) {
 // the game after every change.
 // But: the reload feature is a fragile hack...
 void W_Reload(void) {
-    /* NRFD-EXCLUDE
-    char *filename;
-    lumpindex_t i;
-
-    if (reloadname == NULL)
-    {
-        return;
-    }
-
-    // We must free any lumps being cached from the PWAD we're about to reload:
-    for (i = reloadlump; i < numlumps; ++i)
-    {
-        if (lumpinfo[i]->cache != NULL)
-        {
-            Z_Free(lumpinfo[i]->cache);
-        }
-    }
-
-    // Reset numlumps to remove the reload WAD file:
-    numlumps = reloadlump;
-
-    // Now reload the WAD file.
-    filename = reloadname;
-
-    W_CloseFile(reloadhandle);
-    free(reloadlumps);
-
-    reloadname = NULL;
-    reloadlump = -1;
-    reloadhandle = NULL;
-    W_AddFile(filename);
-    free(filename);
-
-    // The WAD directory has changed, so we have to regenerate the
-    // fast lookup hashtable:
-    W_GenerateHashTable();
-    */
+    printf("NRFD-TODO: W_Reload\n");
 }
-
-/*
-void W_DebugLump(int lump)
-{
-    return;
-    if (lump != 561) return;
-
-    filelump_t *filelump = &filelumps[lump];
-
-    printf("W_DebugLump: %d size: %d\n", lump, filelump->size);
-
-    byte *cache = N_malloc(filelump->size);
-
-    // N_fs_read(wad_file, filelump->filepos, cache, filelump->size);
-    byte *qspi_data = W_LumpDataPointer(lump);
-    for (int i=0;i<filelump->size;i++) {
-        cache[i] = qspi_data[i];
-    }
-
-    debugLumpNums[debugLumpCount] = lump;
-    debugLumpCache[debugLumpCount] = cache;
-    debugLumpCount++;
-}
-*/
