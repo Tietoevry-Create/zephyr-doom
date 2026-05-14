3f8ce17e55a81dc00b142ba716029b2086fdbf7b
Marek <mareklicko448@gmail.com>
2026-05-14 22:03:33 +0200

    joystick field without audio

--- zephyrdoom/src/n_rjoy.c
+++ zephyrdoom/src/n_rjoy.c
@@ -1,127 +1,13 @@
 /*
- * Copyright (c) 2019 - 2020, Nordic Semiconductor ASA
- * All rights reserved.
+ * Joystick backend wrapper.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * This project only needs the wired shield backend on FRDM-MCXN947.
+ * The implementation lives in src/n_rjoy_shield.c.
  */
 
-// Nordic Propriatery gamepad/joycon
+int n_rjoy_backend_init(void);
+void n_rjoy_backend_read(void);
 
-#if defined(CONFIG_SOC_FAMILY_NORDIC_NRF)
+int N_rjoy_init(void) { return n_rjoy_backend_init(); }
 
-#include <stdio.h>
-
-// #include "nrf.h"
-#include <bsp/stable/mdk/nrf.h>
-#include <hal/nrf_clock.h>
-// #include "nrf_delay.h"
-#include "board_config.h"
-
-#undef PACKED_STRUCT
-
-#include "d_event.h"
-#include "doomkeys.h"
-#include "i_system.h"
-
-// State.
-#include "doom/doomstat.h"
-
-const int x_cen = 128;
-const int y_cen = 128;
-const int guard = 4;
-
-extern short st_faceindex;
-
-static player_t* plyr;
-
-typedef struct {
-    uint8_t counter;
-    uint8_t buttons;
-    uint8_t joyX;
-    uint8_t joyY;
-} rjoy_radio_packet_t;
-
-typedef struct {
-    uint8_t face;
-    uint8_t health;
-    uint8_t ammo;
-    uint8_t armor;
-} radio_packet_out;
-
-rjoy_radio_packet_t prev_joy_state;
-
-int N_rjoy_init(void) {
-    plyr = &players[consoleplayer];
-    return 1;
-}
-
-void N_rjoy_read(void) {
-    volatile uint32_t* ipc_ptr = &NRF_IPC_S->GPMEM[0];
-    uint32_t radio_packet = *ipc_ptr;
-    rjoy_radio_packet_t new_joy_state;
-    uint32_t* tmp = (uint32_t*)(&new_joy_state);
-    *tmp = radio_packet;
-    // printf("%d %d %d %d %lx\n", new_joy_state.counter, new_joy_state.buttons,
-    // new_joy_state.joyX, new_joy_state.joyY, *tmp);
-    if (new_joy_state.counter != prev_joy_state.counter) {
-        // printf("N_rjoy_read: %d\n", new_joy_state.counter);
-        event_t ev;
-
-        int joyX = new_joy_state.joyX - x_cen;
-        int joyY = new_joy_state.joyY - y_cen;
-
-        if (-guard < joyX && joyX < guard) joyX = 0;
-        if (-guard < joyY && joyY < guard) joyY = 0;
-
-        ev.type = ev_joystick;
-        ev.data1 = new_joy_state.buttons;
-        ev.data2 = -joyY;
-        ev.data3 = -joyX;
-        ev.data4 = 0;
-        ev.data5 = 0;
-
-        D_PostEvent(&ev);
-    }
-    prev_joy_state = new_joy_state;
-
-    // Provide current face to radio for transfer to gamepad
-    radio_packet_out response = {0};
-    response.face = st_faceindex;
-    response.health = plyr->health;
-    response.ammo = plyr->ammo[weaponinfo[plyr->readyweapon].ammo];
-    response.armor = plyr->armorpoints;
-
-    volatile uint32_t* ipc_ptr_1 = &NRF_IPC_S->GPMEM[1];
-    *ipc_ptr_1 = *(uint32_t*)&response;
-}
-
-#else
-
-int N_rjoy_init(void) { return 0; }
-void N_rjoy_read(void) {}
-
-#endif
+void N_rjoy_read(void) { n_rjoy_backend_read(); }
