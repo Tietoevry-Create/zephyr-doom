db04d5abaee254e6aad514e4e90a50e860d7f8bb
Marek <mareklicko448@gmail.com>
2026-06-05 10:50:41 +0200

    Delete nxp/ unified_implementation/

--- unified_implementation/src/bluetooth_control.h
+++
@@ -1,6 +0,0 @@
-#ifndef BLUETOOTH_CONTROL_H__
-#define BLUETOOTH_CONTROL_H__
-
-int bluetooth_control_init(void);
-
-#endif
--- unified_implementation/src/platform/nxp/n_rjoy_stub.c
+++
@@ -1,4 +0,0 @@
-/* Minimal joystick backend stubs for FRDM builds. */
-
-int N_rjoy_init(void) { return 0; }
-void N_rjoy_read(void) {}
